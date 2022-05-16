520bdc27d653ac9c4800ff94bbc42f01431a93e9
m-schwob <menashe54@gmail.com>
2022-05-16 20:47:56 +0300

    create fucntion for plotting

---
+++ plot_contraction.m
@@ -0,0 +1,12 @@
+% plot the boundary of the convergence
+function plot_contraction(delta_2, omega_2, MU, dist, eq_point)
+    hold on;
+    contourf(delta_2, omega_2, MU, [0, 0]);
+    scatter(eq_point(1), eq_point(2));
+    contour(delta_2, omega_2, dist, [1, 1]);
+    axis square;
+    xlabel('X')
+    ylabel('Y')
+    zlabel('Z')
+    hold off;
+end
